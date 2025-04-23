4460e71a5117d6b5feac92599968453b6ce0e22f
Brandon Taylor <bt@brandontaylor.tech>
2025-04-23 15:21:40 -0400

    deleted flameshot.sh

--- flameshot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# this will run flameshot in scaling mode from cli
-# env QT_AUTO_SCREEN_SCALE_FACTOR=1 flameshot gui
-
-# this will add the fix to the users profile so it persist on reboots
-
-echo "export QT_AUTO_SCREEN_SCALE_FACTOR=1" >> ~/.profile
